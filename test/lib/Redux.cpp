13132dd3919464e0e8a85efc3d54218d7f7a01c6
Peter Halliday <pghalliday@gmail.com>
2020-06-10 19:26:28 +0200

    refactored redux tests to use new BDD framework

--- test/lib/Redux.cpp
+++ test/lib/Redux.cpp
@@ -1,4 +1,5 @@
 #include "./Redux.hpp"
+#include <functional>
 #include <unity.h>
 
 #include <Redux/Action.hpp>
@@ -181,9 +182,34 @@ namespace Redux {
   };
   BarSubscriber barSubscriber;
 
+  class TestSubscriber : public Subscriber<Top> {
+    public:
+      using f_cb = std::function<void(const Top * state)>;
+
+      TestSubscriber() :
+        _cb(nullptr)
+      {}
+
+      void notify(const Top * state) override {
+        if (_cb) {
+          _cb(state);
+          _cb = nullptr;
+        }
+      }
+
+      void callback(f_cb cb) {
+        _cb = cb;
+      }
+
+    private:
+      f_cb _cb;
+  };
+  TestSubscriber testSubscriber;
+
   Subscriber<Top> * subscribers[] = {
     &fooSubscriber,
     &barSubscriber,
+    &testSubscriber,
     nullptr
   };
   SubscriberList<Top> subscriber(subscribers);
@@ -198,28 +224,88 @@ namespace Redux {
       store.setup(&reducer, &subscriber);
     });
 
-    describe.it("should work", []() {
+    describe.it("should have the correct start values", []() {
       TEST_ASSERT_EQUAL_INT(1000, foo);
       TEST_ASSERT_EQUAL_INT(1000, bar);
+    });
+
+    describe.async("after init", [](Async & async, f_done & done) {
       InitialValues initialValues = {
         5, 24
       };
       store.init(initialValues);
-      TEST_ASSERT_EQUAL_INT(5, foo);
-      TEST_ASSERT_EQUAL_INT(24, bar);
+      testSubscriber.callback([&](const Top * state) {
+        async.it("should notify the correct state", [&]() {
+          TEST_ASSERT_EQUAL_INT(5, state->foo->value);
+          TEST_ASSERT_EQUAL_INT(24, state->bar->value);
+        });
+        async.it("should have notified the subscribers in order", [&]() {
+          TEST_ASSERT_EQUAL_INT(5, foo);
+          TEST_ASSERT_EQUAL_INT(24, bar);
+        });
+        done();
+      });
+    });
+
+    describe.async("after increment all", [](Async & async, f_done & done) {
       store.dispatch(incrementAll);
-      TEST_ASSERT_EQUAL_INT(6, foo);
-      TEST_ASSERT_EQUAL_INT(25, bar);
+      testSubscriber.callback([&](const Top * state) {
+        async.it("should notify the correct state", [&]() {
+          TEST_ASSERT_EQUAL_INT(6, state->foo->value);
+          TEST_ASSERT_EQUAL_INT(25, state->bar->value);
+        });
+        async.it("should have notified the subscribers in order", [&]() {
+          TEST_ASSERT_EQUAL_INT(6, foo);
+          TEST_ASSERT_EQUAL_INT(25, bar);
+        });
+        done();
+      });
+    });
+
+    describe.async("after increment foo", [](Async & async, f_done & done) {
       store.dispatch(incrementFoo);
-      TEST_ASSERT_EQUAL_INT(7, foo);
-      TEST_ASSERT_EQUAL_INT(25, bar);
+      testSubscriber.callback([&](const Top * state) {
+        async.it("should notify the correct state", [&]() {
+          TEST_ASSERT_EQUAL_INT(7, state->foo->value);
+          TEST_ASSERT_EQUAL_INT(25, state->bar->value);
+        });
+        async.it("should have notified the subscribers in order", [&]() {
+          TEST_ASSERT_EQUAL_INT(7, foo);
+          TEST_ASSERT_EQUAL_INT(25, bar);
+        });
+        done();
+      });
+    });
+
+    describe.async("after increment bar", [](Async & async, f_done & done) {
       store.dispatch(incrementBar);
-      TEST_ASSERT_EQUAL_INT(7, foo);
-      TEST_ASSERT_EQUAL_INT(26, bar);
+      testSubscriber.callback([&](const Top * state) {
+        async.it("should notify the correct state", [&]() {
+          TEST_ASSERT_EQUAL_INT(7, state->foo->value);
+          TEST_ASSERT_EQUAL_INT(26, state->bar->value);
+        });
+        async.it("should have notified the subscribers in order", [&]() {
+          TEST_ASSERT_EQUAL_INT(7, foo);
+          TEST_ASSERT_EQUAL_INT(26, bar);
+        });
+        done();
+      });
+    });
+
+    describe.async("after increase all", [](Async & async, f_done & done) {
       int increase = 5;
       store.dispatch(Action<TestActionType>(TestActionType::INCREASE_ALL, &increase));
-      TEST_ASSERT_EQUAL_INT(12, foo);
-      TEST_ASSERT_EQUAL_INT(31, bar);
+      testSubscriber.callback([&](const Top * state) {
+        async.it("should notify the correct state", [&]() {
+          TEST_ASSERT_EQUAL_INT(12, state->foo->value);
+          TEST_ASSERT_EQUAL_INT(31, state->bar->value);
+        });
+        async.it("should have notified the subscribers in order", [&]() {
+          TEST_ASSERT_EQUAL_INT(12, foo);
+          TEST_ASSERT_EQUAL_INT(31, bar);
+        });
+        done();
+      });
     });
 
   });
